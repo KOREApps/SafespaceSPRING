a0faac6fab479da5be9a3edf0cda904d276b8cdb
Robert R <rrishaugr@gmail.com>
2017-10-26 20:57:53 +0200

    Initial commit. Empty spring boot prosject

---
+++ src/main/java/no/ntnu/kore/safespace/SafespaceApplication.java
@@ -0,0 +1,12 @@
+package no.ntnu.kore.safespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafespaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafespaceApplication.class, args);
+	}
+}
